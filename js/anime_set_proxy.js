9dfff63de83baac6f72b7a46725226217a81109f
Taxi <xing.tao.1992@gmail.com>
2022-08-21 11:21:49 +0800

    Update anime_set_proxy.js

--- js/anime_set_proxy.js
+++ js/anime_set_proxy.js
@@ -11,6 +11,7 @@ const TARGET_POLICY_GROUP = 'Bili';
 const TARGET_PROXY = 'HK';
 const DIRECT = 'DIRECT';
 const SWITCH_REGEX = /^https:\/\/www\.bilibili\.com\/(anime|bangumi\/play)/;
+const url = 'https://www.bilibili.com/anime/play/';
 // ----------
 
 
@@ -33,32 +34,38 @@ let apiBody = {
     'policy': undefined,  // default?
 }
 let notificationMessage;
-//if (SWITCH_REGEX.test($request.url)) {
+if (SWITCH_REGEX.test(url)) {
     // set policy group to TARGET_PROXY
     apiBody['policy'] = TARGET_PROXY;
     notificationMessage = '开启B站代理';
-//} else {
+} else {
     // set policy group to DIRECT
-//    apiBody['policy'] = DIRECT;
-//    notificationMessage = '关闭B站代理';
-//}
+    apiBody['policy'] = DIRECT;
+    notificationMessage = '关闭B站代理';
+}
 
 
 
 promiseCurrentPolicy.then(currentPolicy => {
     if (currentPolicy === apiBody['policy']) {
         // no need to switch
-        console.log(`${$request.url}，无需切换`);
+        console.log(`${url}，无需切换`);
         $done();
     } else {
         // perform switch
         $httpAPI('POST', '/v1/policy_groups/select', apiBody, (result) => {
             if (result === null)
-                $notification.post(notificationMessage, $request.url, '');
+                {
+                $notification.post(notificationMessage, url, '');
+                console.log(`${url}，${notificationMessage}`);
+                }
             else
+                {
                 // on success: result === null
                 // on failure: result === {"error" : "invalid parameters"}
-                $notification.post(notificationMessage, $request.url, result.error);
+                $notification.post(notificationMessage, url, result.error);
+				console.log(`${url}，${notificationMessage}, error`);
+                }
             $done();
         })
     }
